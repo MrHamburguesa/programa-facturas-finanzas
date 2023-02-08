5906d3249ed318e28ad769d1c04165f995c76640
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-08 12:27:14 -0300

    Crea flujo de lectura de facturas

---
+++ lector_archivos.py
@@ -0,0 +1,11 @@
+class LectorArchivos:
+    def __init__(self):
+        pass
+
+    def leer_bases_de_datos_de_facturas(self):
+        self.leer_sii()
+        self.leer_acepta()
+        self.leer_sigfe()
+        self.leer_turbo()
+        self.leer_sci()
+        self.leer_observaciones()
