f22b69324ff58add2f1d82a73b35da06bb1571b0
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-08 15:39:55 -0300

    Lee archivos basado en diccionario de base de dato

--- lector_archivos.py
+++ lector_archivos.py
@@ -12,29 +12,36 @@ RUTA_TURBO = 'crudos/base_de_datos_facturas/TURBO'
 RUTA_SCI = 'crudos/base_de_datos_facturas/SCI'
 RUTA_OBSERVACIONES = 'crudos/base_de_datos_facturas/OBSERVACIONES'
 
-BASE_FACTURAS = {'SII': SII.leer_un_archivo_sii()}
+BASE_FACTURAS = {'SII': [RUTA_SII, SII().leer_un_archivo_sii]}
 
 
 class LectorArchivos:
     def __init__(self):
         pass
 
+    def leer_todas_las_bases(self, leer_anio_actual=False):
+        self.leer_bases_de_datos_de_facturas(leer_anio_actual)
+        self.leer_bases_de_datos_de_oc(leer_anio_actual)
+        self.leer_bases_de_datos_de_articulos(leer_anio_actual)
+
     def leer_bases_de_datos_de_facturas(self, leer_anio_actual=False):
-        self.leer_sii(leer_anio_actual)
-        self.leer_acepta(leer_anio_actual)
-        self.leer_sigfe(leer_anio_actual)
-        self.leer_turbo(leer_anio_actual)
-        self.leer_sci(leer_anio_actual)
-        self.leer_observaciones(leer_anio_actual)
+        diccionario_bases = {}
+        for base_de_datos, datos_lectura in BASE_FACTURAS.items():
+            ruta_de_base, funcion_lectora = datos_lectura[0], datos_lectura[1]
+            df_completo_base = self.concatenar_dfs(ruta_de_base, funcion_lectora, leer_anio_actual)
+            diccionario_bases[base_de_datos] = df_completo_base
+
+        return diccionario_bases
 
     def leer_todos_los_archivos_sii(self, leer_anio_actual):
         archivos = self.obtener_archivos_contenidos_en_carpeta(RUTA_SII, leer_anio_actual)
         dfs = pd.concat(map(lambda x: self.leer_un_archivo_sii(x), archivos))
         return dfs
 
-    def concatenar_dfs(self, archivos_a_leer, funcion_lectura_base_de_datos, leer_anio_actual):
-        archivos = self.obtener_archivos_contenidos_en_carpeta(archivos_a_leer, leer_anio_actual)
-        dfs = pd.concat(map(lambda x: funcion_lectura_base_de_datos(x), archivos))
+    def concatenar_dfs(self, ruta_directorio_superior, funcion_lectura, leer_anio_actual):
+        archivos = self.obtener_archivos_contenidos_en_carpeta(
+            ruta_directorio_superior, leer_anio_actual)
+        dfs = pd.concat(map(lambda x: funcion_lectura(x), archivos))
         return dfs
 
     def filtrar_por_anio_actual(self, lista_archivos_contenidos):
@@ -55,4 +62,4 @@ class LectorArchivos:
 
 if __name__ == '__main__':
     objeto = LectorArchivos()
-    print(objeto.leer_todos_los_archivos_sii(False).to_csv('sii.csv', sep=';'))
+    df = objeto.leer_bases_de_datos_de_facturas(False)
