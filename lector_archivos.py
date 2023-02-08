595ac2f0091fbcfa960f3f58fbee040957439174
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-08 14:41:21 -0300

    Lee archivos del SII con cols de tabaco

--- lector_archivos.py
+++ lector_archivos.py
@@ -1,6 +1,8 @@
 import os
 from datetime import date
 
+import pandas as pd
+
 RUTA_SII = 'crudos/base_de_datos_facturas/SII'
 RUTA_ACEPTA = 'crudos/base_de_datos_facturas/ACEPTA'
 RUTA_SIGFE_REPORTS = 'crudos/base_de_datos_facturas/SIGFE'
@@ -21,8 +23,17 @@ class LectorArchivos:
         self.leer_sci(leer_anio_actual)
         self.leer_observaciones(leer_anio_actual)
 
-    def leer_sii(self, leer_anio_actual):
-        archivos = self.obtener_archivos_contenidos_en_carpeta()
+    def leer_un_archivo_sii(self, ruta_archivo):
+        df = pd.read_csv(ruta_archivo, delimiter=';', index_col=False)
+        df = df.drop(columns=['Tabacos Puros', 'Tabacos Cigarrillos', 'Tabacos Elaborados'])
+
+        return df
+
+    def leer_todos_los_archivos_sii(self, leer_anio_actual):
+        archivos = self.obtener_archivos_contenidos_en_carpeta(RUTA_SII, leer_anio_actual)
+
+        dfs = map(lambda x: pd.read_csv(x, delimiter=';', index_col=False), archivos)
+
         pass
 
     def filtrar_por_anio_actual(self, lista_archivos_contenidos):
@@ -42,3 +53,5 @@ class LectorArchivos:
 if __name__ == '__main__':
     objeto = LectorArchivos()
     print(objeto.obtener_archivos_contenidos_en_carpeta('crudos/base_de_datos_facturas/SII', True))
+    print(objeto.leer_un_archivo_sii(
+        'crudos/base_de_datos_facturas/SII/RCV_COMPRA_REGISTRO_61608402-K_202103.csv'))
