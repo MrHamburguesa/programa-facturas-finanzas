17af3ef50147c479bd599a3c2767ffffa56ca17d
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-02-08 12:37:48 -0300

    Lee archivos contenidos en carpeta

--- lector_archivos.py
+++ lector_archivos.py
@@ -1,3 +1,6 @@
+import os
+from datetime import date
+
 class LectorArchivos:
     def __init__(self):
         pass
@@ -9,3 +12,23 @@ class LectorArchivos:
         self.leer_turbo()
         self.leer_sci()
         self.leer_observaciones()
+
+    def filtrar_por_anio_actual(self, lista_archivos_contenidos):
+        anio_actual = str(date.today().year)
+        archivos_filtrados = list(filter(lambda x: anio_actual in x,  lista_archivos_contenidos))
+        return archivos_filtrados
+
+    def obtener_archivos_contenidos_en_carpeta(self, carpeta_a_leer, filtro_anio=False):
+        lista_archivos_contenidos = os.listdir(carpeta_a_leer)
+
+        if filtro_anio:
+            lista_archivos_contenidos = self.filtrar_por_anio_actual(lista_archivos_contenidos)
+
+        return lista_archivos_contenidos
+
+    def leer_sii(self):
+        pass
+
+if __name__ == '__main__':
+    objeto = LectorArchivos()
+    print(objeto.obtener_archivos_contenidos_en_carpeta('crudos/base_de_datos_facturas/SII'))
